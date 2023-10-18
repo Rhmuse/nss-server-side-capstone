aa49ec47e93790486bd679080d0bba9a68ee529c
Rhmuse <rhett.muse123@gmail.com>
2023-10-18 15:27:16 -0500

    add userDetails and clock to employee view

---
+++ client/src/components/Clock.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const date = new Date();
+
+const Clock = () => {
+    const [dateTime, setDateTime] = useState({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+    });
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const date = new Date();
+
+            setDateTime({
+                hours: date.getHours(),
+                minutes: date.getMinutes(),
+                seconds: date.getSeconds()
+            });
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <>
+            {dateTime.hours}:{dateTime.minutes}:{dateTime.seconds}
+        </>
+    )
+}
+
+export default Clock; 
