758a299e32a09f6757806bd9d2ea3601e1712c01
Rhmuse <rhett.muse123@gmail.com>
2023-10-20 09:57:57 -0500

    add context to emplyeeview

--- client/src/components/views/EmployeeView.js
+++ client/src/components/views/EmployeeView.js
@@ -1,15 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
 import { AuthorizedRoute } from '../auth/AuthorizedRoute';
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { logout } from '../../managers/authManager';
+import { createContext } from 'react';
 import OrderSummary from '../pos/OrderSummary';
 import PreviousOrdersList from '../pos/PreviousOrdersList';
 import LoggedInUserDetails from '../pos/LoggedInUserDetails';
 import MainPosView from '../pos/MainPosView';
-import { logout } from '../../managers/authManager';
 
 import "./EmployeeView.css";
 
 const EmployeeView = ({ loggedInUser, setLoggedInUser }) => {
+
+    const OrderContext = createContext(
+        {
+            order: {
+                orderTypeId: "",
+                customerId: "",
+                employeeId: ""
+            },
+            drinks: {},
+            burgers: {},
+            sides: {}
+        }
+    );
+
     return (
         <Routes>
             <Route path="/">
@@ -17,37 +32,39 @@ const EmployeeView = ({ loggedInUser, setLoggedInUser }) => {
                     index
                     element={
                         <AuthorizedRoute loggedInUser={loggedInUser}>
-                            <Container >
-                                <Row>
-                                    <Col lg md sm xl="2" >
-                                        <Row>
-                                            <OrderSummary />
-                                        </Row>
-                                        <Row>
-                                            <PreviousOrdersList />
-                                        </Row>
-                                    </Col>
-                                    <Col>
-                                        <Row id='userDetailsRow'>
-                                            <Button
-                                                color="primary"
-                                                onClick={(e) => {
-                                                    e.preventDefault();
-                                                    logout().then(() => {
-                                                        setLoggedInUser(null);
-                                                    });
-                                                }}
-                                            >
-                                                Logout
-                                            </Button>
-                                            <LoggedInUserDetails loggedInUser={loggedInUser} />
-                                        </Row>
-                                        <Row>
-                                            <MainPosView />
-                                        </Row>
-                                    </Col>
-                                </Row>
-                            </Container>
+                            <OrderContext.Provider>
+                                <Container >
+                                    <Row>
+                                        <Col lg md sm xl="2" >
+                                            <Row>
+                                                <OrderSummary />
+                                            </Row>
+                                            <Row>
+                                                <PreviousOrdersList />
+                                            </Row>
+                                        </Col>
+                                        <Col>
+                                            <Row id='userDetailsRow'>
+                                                <Button
+                                                    color="primary"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        logout().then(() => {
+                                                            setLoggedInUser(null);
+                                                        });
+                                                    }}
+                                                >
+                                                    Logout
+                                                </Button>
+                                                <LoggedInUserDetails loggedInUser={loggedInUser} />
+                                            </Row>
+                                            <Row>
+                                                <MainPosView />
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </OrderContext.Provider>
                         </AuthorizedRoute>
                     }
                 />
