e64e6abc1e90b5c89875c21c85ee2e3465de1bb5
Rhmuse <rhett.muse123@gmail.com>
2023-11-02 12:59:04 -0500

    display previousOrders

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -67,7 +67,6 @@ public class OrdersController : ControllerBase
 
             };
             _dbContext.OrderDrinks.Add(newOrderDrink);
-            System.Console.WriteLine("stop");
         });
 
         orderDto.OrderSideDtos.ForEach(s =>
